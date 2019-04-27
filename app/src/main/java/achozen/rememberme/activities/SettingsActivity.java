92ce8de497ece0c5125d45f011b7b5552412b3ff
Achozen <achozen93@gmail.com>
2019-04-27 23:58:31 +0200

    Small refactor

--- app/src/main/java/achozen/rememberme/activities/SettingsActivity.java
+++ app/src/main/java/achozen/rememberme/activities/SettingsActivity.java
@@ -1,10 +1,14 @@
 package achozen.rememberme.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import achozen.rememberme.R;
 import achozen.rememberme.engine.PeferencesUtil;
@@ -17,6 +21,9 @@ public class SettingsActivity extends Activity {
     @BindView(R.id.inputText)
     TextInputEditText editText;
 
+    @BindView(R.id.loggedInValue)
+    TextView loggedInVaue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +33,26 @@ public class SettingsActivity extends Activity {
         if (!"UNKNOWN".equalsIgnoreCase(userName)) {
             editText.setText(userName);
         }
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null && currentUser.getEmail() != null) {
+            loggedInVaue.setText(currentUser.getEmail());
+        }
     }
+
 
     @OnClick(R.id.saveButton)
     void saveButtonClicked(View v) {
         PeferencesUtil.storeInPrefs(this, PeferencesUtil.Preferences.USERNAME, editText.getText().toString());
         finish();
+    }
+
+    @OnClick(R.id.logoutButton)
+    void onLogoutClicked(View v) {
+        PeferencesUtil.storeInPrefs(this, PeferencesUtil.Preferences.USERNAME, PeferencesUtil.UNKNOWN_USERNAME);
+        FirebaseAuth.getInstance().signOut();
+        finishAffinity();
+        Intent startupIntent = new Intent(this, SplashScreenActivity.class);
+        startActivity(startupIntent);
     }
 }
